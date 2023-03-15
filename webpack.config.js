a227a9c8db126c067a02d0a61ec411ba11436e3b
Wesley Freitas <wesley8bu@gmail.com>
2023-03-14 23:49:01 -0300

    atualizado

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const path = require("path")
 module.exports = {
     mode: "development",
     target: ["web", "es5"],
-    // entry: ["core-js/stable", "./src/js/index.ts"], // gera mais de 10 mil linhas
     entry: ["./src/js/index.ts"],
     output: {
         path: path.resolve(__dirname, "dist", "js"),
